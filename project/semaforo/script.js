21e41b42b783cc1527e4769160cfc7fb7e973e87
FernandinOficial <fernando.furibeiro@gmail.com>
2024-09-26 22:08:56 -0300

    semáforo pronto, faltando modo automatico

---
+++ project/semaforo/script.js
@@ -0,0 +1,32 @@
+
+function autoButton(){
+
+}
+
+function redButton(){
+    var redLight = document.getElementById('luz-vermelha');
+    var yellowLight = document.getElementById('luz-amarela');
+    var greenLight = document.getElementById('luz-verde');
+    
+    redLight.style.backgroundColor = '#f00';
+    yellowLight.style.backgroundColor = '#87870f';
+    greenLight.style.backgroundColor = '#0a6e0a';
+}
+function yellowButton(){
+    var redLight = document.getElementById('luz-vermelha');
+    var yellowLight = document.getElementById('luz-amarela');
+    var greenLight = document.getElementById('luz-verde');
+    
+    redLight.style.backgroundColor = '#6e0a0a';
+    yellowLight.style.backgroundColor = '#ff0';
+    greenLight.style.backgroundColor = '#0a6e0a';
+}
+function greenButton(){
+    var redLight = document.getElementById('luz-vermelha');
+    var yellowLight = document.getElementById('luz-amarela');
+    var greenLight = document.getElementById('luz-verde');
+    
+    redLight.style.backgroundColor = '#6e0a0a';
+    yellowLight.style.backgroundColor = '#87870f';
+    greenLight.style.backgroundColor = '#0f0';
+}
